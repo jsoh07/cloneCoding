3cd33d4604a27b5dbd9ea194704a2a5ab6d18d8d
jaeseok <jsoh07@sharemind.kr>
2024-08-20 15:46:41 +0900

    초기셋팅

---
+++ src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+    return (
+        <main>
+            로그인모달
+        </main>
+    );
+}
---
+++ src/app/(beforeLogin)/i/flow/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+    return (
+        <main>
+            회원가입모달
+        </main>
+    );
+}
