1cf259915221e431af645ee995ec70ecc23edec9
jaeseok <jsoh07@sharemind.kr>
2024-08-22 20:19:56 +0900

    before login 까지

--- src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
+++ src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
@@ -2,6 +2,10 @@ import LoginModal from "@/app/(beforeLogin)/_component/LoginModal";
 
 export default function Page() {
   return (
-    <LoginModal />
+    <>
+    난 가로채기지롱
+      <LoginModal />
+    </>
+    
   );
 }
